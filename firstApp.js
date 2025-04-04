1497d6e8abb041f14bc01a746ce508609b91feca
Teodoro Montanaro <teodoro.montanaro@unisalento.it>
2025-04-04 16:54:47 +0200

    working version

--- firstApp.js
+++ firstApp.js
@@ -1,32 +1,24 @@
-const sensorLib = require('node-dht-sensor'); // include existing module called ‘node-dht-sensor’
-const http = require('node:http');
+const mqtt=require('mqtt');
 
-// Setup sensor, exit if failed
-var sensorType = 11; // 11 for DHT11, 22 for DHT22 and AM2302
-var sensorPin = 4; // The GPIO pin number for sensor signal
-if (!sensorLib.initialize(sensorType, sensorPin))
-{
-//print a warning message in the console
-console.warn('Failed to initialize sensor');
-process.exit(1);
-}
+var client = mqtt.connect("mqtt://mqtt.eclipseprojects.io",{clientId:"mqttjs01"});
+client.on("connect",function(){
+    console.log("connected");
+});
+client.on("error",function(error){
+    console.log("Can't connect"+error);
+});
 
 // Automatically update sensor value every 2 seconds
 //we use a nested function (function inside another function)
 setInterval(function() {
-	var readout = sensorLib.read();
-
-	console.log('Temperature:', readout.temperature.toFixed(1) + 'C');
-	console.log('Humidity: ', readout.humidity.toFixed(1) + '%');
-	var temperature = readout.temperature.toFixed(1);
 
 	const postData = JSON.stringify({
 		'sensor': 'ID1',
 		'timestamp': 12345678,
-		'temperature': temperature
+		'temperature': Math.random()
 	})
 	const options = {
-	hostname: '192.168.137.1',
+	hostname: 'localhost',
 	port: 3000,
 	path: '/temperature',
 	method: 'POST',
@@ -36,21 +28,6 @@ setInterval(function() {
 	},
 	};
 
-	const req = http.request(options, (res) => {
-		res.setEncoding('utf8');
-		res.on('data', (chunk) => {
-		  console.log(`BODY: ${chunk}`);
-		});
-		res.on('end', () => {
-		  console.log('No more data in response.');
-		});
-	});
+	client.publish("test-topic-handson/data", postData);
 
-	req.on('error', (e) => {
-		console.error(`problem with request: ${e.message}`);
-	});
-
-	// Write data to request body
-	req.write(postData);
-	req.end();
 }, 2000);
